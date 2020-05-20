fada5987d28b3a4ae1913a6db015966aaf56645e
yulongdong <longdong.yu@zhenai.com>
2020-05-20 18:07:02 +0800

    动态路由实现

--- dont-setting-router/src/router/index.js
+++ dont-setting-router/src/router/index.js
@@ -9,16 +9,12 @@ const insert = (url) => {
   let urlArr = url.slice(2).split('/')
   //移除vue文件
   urlArr.pop()
-
-  console.log(urlArr)
-
   let node = routes
   let path = ''
   urlArr.forEach((item, index) => {
     let humpItem = item
     item = humpToLine(item)
     path = index ? path : ''
-    console.log(node.some((val) => val.name === item))
     if (node.some((val) => val.name === item)) {
       for (let i = 0; i < node.length; i++) {
         if (node[i].name === item) {
@@ -47,12 +43,7 @@ routerArr.forEach((element) => {
   insert(element)
 })
 
-console.log(routes.concat({
-	path: '',
-	name: 'home_index',
-	redirect: '/home_index',
-	component: () => import(`@/views/homeIndex/homeIndex.vue`),
-}),)
+//加上默认重定向
 routes = routes.concat({
 	path: '',
 	name: 'home_index',
@@ -61,35 +52,4 @@ routes = routes.concat({
 })
 export default new Router({
   routes
-  // routes: [
-  //   {
-  //     path: '',
-  //     name: 'home_index',
-  //     redirect: '/home_index',
-  //     component: () => import(aa),
-  //   },
-  //   {
-  //     path: '/home_index',
-  //     name: 'home_index',
-  //     component: () => import(aa),
-  //   },
-  //   {
-  //     path: '/home_index',
-  //     name: 'home_index',
-  //     component: () => import(aa),
-  //     children: [
-  //       {
-  //         path: 'index_children',
-  //         name: 'index_children',
-  //         component: () =>
-  //           import(`@/views/homeIndex/indexChildren/indexChildren.vue`),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/home_category',
-  //     name: 'home_category',
-  //     component: () => import(`@/views/homeCategory/homeCategory.vue`),
-  //   },
-  // ],
 })
